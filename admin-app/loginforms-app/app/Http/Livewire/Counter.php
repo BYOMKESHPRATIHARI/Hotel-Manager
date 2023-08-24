4965f137a5d106743e6406f5d33ae8bd5af9bba9
BYOMKESH PRATIHARI <35007803+BYOMKESHPRATIHARI@users.noreply.github.com>
2023-08-24 14:57:42 +0000

    origin/main

---
+++ admin-app/loginforms-app/app/Http/Livewire/Counter.php
@@ -0,0 +1,17 @@
+<?php
+class Counter extends Component
+{
+    public $count = 0;
+ 
+    public function increment()
+    {
+        $this->count++;
+    }
+ 
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
+
+?>
